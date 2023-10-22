1a9aecc5924918b9fae758f3a297a208b8130836
Gbgarj <133577037+Gbgarj@users.noreply.github.com>
2023-10-23 01:35:25 +0800

    Add files via upload

---
+++ Arithmetic_Input_Operators.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Arithmetic_Input_Operators {
+public static void main(String[] args) {
+String x;
+Scanner scan = new Scanner( System.in); 
+    System.out.println(" Enter your name: ");
+    x = scan.nextLine();
+
+System.out.println(" You are "+ x);
+
+}
+}
